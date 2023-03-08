2c9bb9ea1ad47466fd83c0a6e5ca2e13675b9a89
Angel Torres <119983420+Angeltorx@users.noreply.github.com>
2023-03-07 22:17:47 -0500

    1 html y JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,28 @@
 console.log("Entro index.js");
+
+(() => {
+    // TODO EL CODIGO VA AQUI EN LA FUNCION ANONIMA
+
+    console.log("Console log desde funcion anonima");
+    document.getElementById("convertButton").addEventListener("click", convertir);
+    document.getElementById("informationButton").addEventListener("click", showInformation);
+
+    const currencyFrom = document.getElementById("currencyFrom");
+    const currencyTo = document.getElementById("currencyTo");
+    const amountFrom = document.getElementById("amountFrom");
+    const amountTo = document.getElementById("amountTo");
+
+    function convertir() {
+        console.log("Se realizo un click en el boton convertir.");
+        console.log(currencyFrom.Value, "currencyFrom")
+        console.log(currencyTo.Value, "currencyTo")
+        console.log(currencyFrom.Value, "amountFrom")
+        console.log(currencyto.Value, "amountTo")
+    }
+
+    function showInformation() {
+        console.log("Se realizo un click en el boton informacion");
+    }
+
+})()
+
